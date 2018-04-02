6e764cea163591d7eac7561ca70fd7fb5465c56e
Joy Paglinawan <virliejoyp@yahoo.com>
2018-04-01 21:52:30 -0700

    Set up the codes for jscript.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,18 @@ function Contact (first, last) {
     this.firstName = first;
     this.lastName = last;
 }
+$(document.ready(function(){
+  $("form#new-contact").submit(function(event){
+    e.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastNmae = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class="contact"> + newContact.firstName + "</span></li>);
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
